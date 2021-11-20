0aad5e24d705915668a6384fd5c7d540d3c771cb
amigma00 <ali.aliaman786@gmail.com>
2021-11-20 19:34:37 +0530

    done application

--- lib/View/dropdown/hospital.dart
+++ lib/View/dropdown/hospital.dart
@@ -1,5 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:collegeproject/utilities/list.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 class Hospital extends StatefulWidget {
@@ -14,81 +17,161 @@ class _HospitalState extends State<Hospital> {
   bool state = false;
 
   @override
+  void initState() {
+    getData();
+  }
+
+  String city = " ";
+  String pinCode = " ";
+
+  getData() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      city = prefs.getString('city')!;
+      pinCode = prefs.getString('pinCode')!;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     var W = context.safePercentWidth;
     var H = context.safePercentHeight;
     return Expanded(
-      child: ListView.builder(
-          itemCount: ok.length,
-          itemBuilder: (context, index) {
-            return Container(
-              child: Card(
-                elevation: 5,
-                //color: Colors.grey,
-                //margin: EdgeInsets.all(10),
-                child: Container(
-                  padding: EdgeInsets.all(W * 2),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        mainAxisAlignment:
-                        MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Column(
-                            crossAxisAlignment:
-                            CrossAxisAlignment.start,
+      child: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance
+              .collection('Hospital')
+              .where(
+                'city',
+                isEqualTo: city,
+              )
+              .where('pincode', isEqualTo: pinCode)
+              .snapshots(),
+          builder: (context, hospSnapshot) {
+            if (hospSnapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            } else {
+              final hospDocs = hospSnapshot.data!.docs;
+
+              return ListView.builder(
+                  itemCount: hospDocs.length,
+                  itemBuilder: (context, index) {
+                    Future<void> openMap(
+                        double latitude, double longitude) async {
+                      String googleUrl =
+                          'https://www.google.com/maps/search/?api=1&query=$latitude,$longitude';
+                      if (await canLaunch(googleUrl)) {
+                        await launch(googleUrl);
+                      } else {
+                        throw 'Could not open the map.';
+                      }
+                    }
+
+                    Future<void> _makePhoneCall(String url) async {
+                      if (await canLaunch(url)) {
+                        await launch(url);
+                      } else {
+                        throw 'Could not launch $url';
+                      }
+                    }
+
+                    var k = hospDocs[index]['phoneno'];
+                    bool ok = hospDocs[index]['status'];
+                    return Container(
+                      child: Card(
+                        elevation: 5,
+                        //color: Colors.grey,
+                        //margin: EdgeInsets.all(10),
+                        child: Container(
+                          padding: EdgeInsets.all(W * 2),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              "ABC Hospital".text.xl2.bold.make(),
                               Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
-                                  "Available Beds : ".text.make(),
-                                  "34".text.green400.make(),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(hospDocs[index]['name'])
+                                          .text
+                                          .xl2
+                                          .bold
+                                          .make(),
+                                      Row(
+                                        children: [
+                                          "Available Beds : ".text.make(),
+                                          Text(hospDocs[index]['beds'])
+                                              .text
+                                              .green400
+                                              .make(),
+                                        ],
+                                      ),
+                                      HeightBox(20),
+                                      Row(
+                                        children: [
+                                          IconButton(
+                                              onPressed: () {
+                                                openMap(
+                                                    hospDocs[index]['latitude'],
+                                                    hospDocs[index]
+                                                        ['longitude']);
+                                              },
+                                              icon: Icon(Icons.directions)),
+                                          IconButton(
+                                              onPressed: () {
+                                                _makePhoneCall('tel:+91$k');
+                                              },
+                                              icon: Icon(Icons.phone)),
+                                        ],
+                                      )
+                                    ],
+                                  ),
+                                  Column(
+                                    children: [
+                                      Row(
+                                        children: [
+                                          "Status : ".text.make(),
+                                          if (state == ok)
+                                            "Off".text.red400.bold.make()
+                                          else
+                                            "On".text.green400.bold.make()
+                                        ],
+                                      ),
+                                      HeightBox(10),
+                                      hospDocs[index]['piclink']!=null
+                                          ?ClipRRect(
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(10)),
+                                        child: Image.network(
+                                          hospDocs[index]['piclink'],
+                                          scale: 2,
+                                          height: H*20,
+                                          width: W*20,
+                                        ),
+                                      ):ClipRRect(
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(10)),
+                                        child: Image.network(
+                                          "assets/logos/download.jfif",
+                                          scale: 2,
+                                          height: H*20,
+                                          width: W*20,
+                                        ),
+                                      ),
+                                    ],
+                                  )
                                 ],
                               ),
-                              HeightBox(20),
-                              Row(
-                                children: [
-                                  IconButton(
-                                      onPressed: () {},
-                                      icon: Icon(Icons.directions)),
-                                  IconButton(
-                                      onPressed: () {},
-                                      icon: Icon(Icons.phone)),
-                                ],
-                              )
                             ],
                           ),
-                          Column(
-                            children: [
-                              Row(
-                                children: [
-                                  "Status : ".text.make(),
-                                  if (state == true)
-                                    "On".text.green400.bold.make()
-                                  else
-                                    "Off".text.red400.bold.make(),
-                                ],
-                              ),
-                              HeightBox(10),
-                              ClipRRect(
-                                borderRadius: BorderRadius.all(
-                                    Radius.circular(10)),
-                                child: Image.asset(
-                                  "assets/logos/download.jfif",
-                                  scale: 2,
-                                ),
-                              ),
-                            ],
-                          )
-                        ],
+                        ),
                       ),
-                    ],
-                  ),
-                ),
-              ),
-            );
+                    );
+                  });
+            }
           }),
     );
   }
--- lib/View/dropdown/shop.dart
+++ lib/View/dropdown/shop.dart
@@ -1,6 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:collegeproject/utilities/list.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:velocity_x/velocity_x.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Shop extends StatefulWidget {
   const Shop({Key? key}) : super(key: key);
@@ -14,83 +17,155 @@ class _ShopState extends State<Shop> {
   var color = Colors.red;
 
   @override
+  void initState() {
+    getData();
+  }
+
+  String city = " ";
+  String pinCode = " ";
+
+  getData() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      city = prefs.getString('city')!;
+      pinCode = prefs.getString('pinCode')!;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     var W = context.safePercentWidth;
     var H = context.safePercentHeight;
     return Expanded(
-      child: ListView.builder(
-          itemCount: ok.length,
-          itemBuilder: (context, index) {
-            return Container(
-              child: Card(
-                elevation: 5,
-                //color: Colors.grey,
-                //margin: EdgeInsets.all(10),
-                child: Container(
-                  padding: EdgeInsets.all(W * 2),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        mainAxisAlignment:
-                        MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Column(
-                            crossAxisAlignment:
-                            CrossAxisAlignment.start,
+      child: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance
+          .collection('Shop')
+          .where('city', isEqualTo: city,)
+          .where('pincode', isEqualTo: pinCode)
+          .snapshots(),
+          builder: (context, shopSnapshot) {
+            if (shopSnapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            } else {
+              final shopDocs = shopSnapshot.data!.docs;
+              return ListView.builder(
+                  itemCount: shopDocs.length,
+                  itemBuilder: (context, index) {
+                    Future<void> openMap(double latitude,
+                        double longitude) async {
+                      String googleUrl =
+                          'https://www.google.com/maps/search/?api=1&query=$latitude,$longitude';
+                      if (await canLaunch(googleUrl)) {
+                        await launch(googleUrl);
+                      } else {
+                        throw 'Could not open the map.';
+                      }
+                    }
+
+                    Future<void> _makePhoneCall(String url) async {
+                      if (await canLaunch(url)) {
+                        await launch(url);
+                      } else {
+                        throw 'Could not launch $url';
+                      }
+                    }
+                    var k = shopDocs[index]['phoneno'];
+                    bool ok = shopDocs[index]['status'];
+                    return Container(
+                      child: Card(
+                        elevation: 5,
+                        //color: Colors.grey,
+                        //margin: EdgeInsets.all(10),
+                        child: Container(
+                          padding: EdgeInsets.all(W * 2),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              "Grocery Shop".text.xl2.bold.make(),
-                              // Row(
-                              //   children: [
-                              //     "Available Beds : ".text.make(),
-                              //     "34".text.green400.make(),
-                              //   ],
-                              // ),
-                              HeightBox(20),
                               Row(
+                                mainAxisAlignment:
+                                MainAxisAlignment.spaceBetween,
+                                crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
-                                  IconButton(
-                                      onPressed: () {},
-                                      icon: Icon(Icons.directions)),
-                                  IconButton(
-                                      onPressed: () {},
-                                      icon: Icon(Icons.phone)),
+                                  Column(
+                                    crossAxisAlignment:
+                                    CrossAxisAlignment.start,
+                                    children: [
+                                      Text(shopDocs[index]['name'])
+                                          .text
+                                          .xl2
+                                          .bold
+                                          .make(),
+                                      // Row(
+                                      //   children: [
+                                      //     "Available Beds : ".text.make(),
+                                      //     "34".text.green400.make(),
+                                      //   ],
+                                      // ),
+                                      HeightBox(20),
+                                      Row(
+                                        children: [
+                                          IconButton(
+                                              onPressed: () {
+                                                openMap(
+                                                    shopDocs[index]['latitude'],
+                                                    shopDocs[index]
+                                                    ['longitude']);
+                                              },
+                                              icon: Icon(Icons.directions)),
+                                          IconButton(
+                                              onPressed: () {
+                                                _makePhoneCall(
+                                                    'tel:+91$k');
+                                              },
+                                              icon: Icon(Icons.phone)),
+                                        ],
+                                      )
+                                    ],
+                                  ),
+                                  Column(
+                                    children: [
+                                      Row(
+                                        children: [
+                                          "Status : ".text.make(),
+                                          if (state == ok)
+                                            "On".text.green400.bold.make()
+                                          else
+                                            "Off".text.red400.bold.make(),
+                                        ],
+                                      ),
+                                      HeightBox(10),
+                                      shopDocs[index]['piclink']!=null
+                                      ?ClipRRect(
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(10)),
+                                        child: Image.network(
+                                          shopDocs[index]['piclink'],
+                                          scale: 2,
+                                          height: H*20,
+                                          width: W*20,
+                                        ),
+                                      ):ClipRRect(
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(10)),
+                                        child: Image.network(
+                                          "assets/logos/grocery.jpg",
+                                          scale: 2,
+                                          height: H*20,
+                                          width: W*20,
+                                        ),
+                                      ),
+                                    ],
+                                  )
                                 ],
-                              )
+                              ),
                             ],
                           ),
-                          Column(
-                            children: [
-                              Row(
-                                children: [
-                                  "Status : ".text.make(),
-                                  if (state == true)
-                                    "On".text.green400.bold.make()
-                                  else
-                                    "Off".text.red400.bold.make(),
-                                ],
-                              ),
-                              HeightBox(10),
-                              ClipRRect(
-                                borderRadius: BorderRadius.all(
-                                    Radius.circular(10)),
-                                child: Image.asset(
-                                  "assets/logos/grocery.jpg",
-                                  scale: 2,
-                                ),
-                              ),
-                            ],
-                          )
-                        ],
+                        ),
                       ),
-                    ],
-                  ),
-                ),
-              ),
-            );
+                    );
+                  });
+            }
           }),
     );
   }
 }
-
